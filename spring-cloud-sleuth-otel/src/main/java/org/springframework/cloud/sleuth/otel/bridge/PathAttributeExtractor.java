e74cd45b12c7af22dbda72fbe9f1ca6f2f4b80f9
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2022-05-20 13:14:37 +0200

    Bumped OTel to 1.14.0; fixes gh-108

--- spring-cloud-sleuth-otel/src/main/java/org/springframework/cloud/sleuth/otel/bridge/PathAttributeExtractor.java
+++ spring-cloud-sleuth-otel/src/main/java/org/springframework/cloud/sleuth/otel/bridge/PathAttributeExtractor.java
@@ -18,12 +18,12 @@ package org.springframework.cloud.sleuth.otel.bridge;
 
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.AttributesBuilder;
+import io.opentelemetry.context.Context;
 import io.opentelemetry.instrumentation.api.instrumenter.AttributesExtractor;
 import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
 
 import org.springframework.cloud.sleuth.http.HttpRequest;
 import org.springframework.cloud.sleuth.http.HttpResponse;
-import org.springframework.lang.Nullable;
 import org.springframework.util.StringUtils;
 
 class PathAttributeExtractor implements AttributesExtractor<HttpRequest, HttpResponse> {
@@ -31,21 +31,21 @@ class PathAttributeExtractor implements AttributesExtractor<HttpRequest, HttpRes
 	private static final AttributeKey<String> HTTP_PATH = AttributeKey.stringKey("http.path");
 
 	@Override
-	public void onStart(AttributesBuilder attributes, HttpRequest httpRequest) {
+	public void onStart(AttributesBuilder attributes, Context parentContext, HttpRequest httpRequest) {
 		String path = httpRequest.path();
 		if (StringUtils.hasLength(path)) {
 			// TODO some tests expect this even on client spans, but this goes against
 			// Otel semantic conventions
 			// should fix tests
-			set(attributes, SemanticAttributes.HTTP_ROUTE, path);
+			attributes.put(SemanticAttributes.HTTP_ROUTE, path);
 			// some tests from Sleuth expect http.route attribute and some http.path
-			set(attributes, HTTP_PATH, path);
+			attributes.put(HTTP_PATH, path);
 		}
 	}
 
 	@Override
-	public void onEnd(AttributesBuilder attributes, HttpRequest httpRequest, @Nullable HttpResponse httpResponse,
-			@Nullable Throwable error) {
+	public void onEnd(AttributesBuilder attributes, Context context, HttpRequest httpRequest, HttpResponse httpResponse,
+			Throwable error) {
 
 	}
 
